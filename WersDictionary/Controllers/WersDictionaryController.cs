0052943930e4ff8b9c7585111a23cdf457561183
Pete Fitzpatrick <pete@prf.demon.co.uk>
2014-02-14 16:44:51 +0000

    add model (EF)

---
+++ WersDictionary/Controllers/WersDictionaryController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WersDictionary.Models;
+
+namespace WersDictionary.Controllers
+{
+    public class WersDictionaryController : Controller
+    {
+        private WersItemDBContext db = new WersItemDBContext();
+
+        //
+        // GET: /WersDictionary/
+
+        public ActionResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        //
+        // GET: /WersDictionary/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            WersItem wersitem = db.Movies.Find(id);
+            if (wersitem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wersitem);
+        }
+
+        //
+        // GET: /WersDictionary/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /WersDictionary/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(WersItem wersitem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(wersitem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(wersitem);
+        }
+
+        //
+        // GET: /WersDictionary/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            WersItem wersitem = db.Movies.Find(id);
+            if (wersitem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wersitem);
+        }
+
+        //
+        // POST: /WersDictionary/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(WersItem wersitem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(wersitem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(wersitem);
+        }
+
+        //
+        // GET: /WersDictionary/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            WersItem wersitem = db.Movies.Find(id);
+            if (wersitem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(wersitem);
+        }
+
+        //
+        // POST: /WersDictionary/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            WersItem wersitem = db.Movies.Find(id);
+            db.Movies.Remove(wersitem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WersDictionary/Models/WersItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WersDictionary.Models
+{
+    public class WersItem
+    {
+        public int ID { get; set; }
+        public string FamilyCode { get; set; }
+        public string FamilyDescription { get; set; }
+    }
+
+    public class WersItemDBContext : DbContext
+    {
+        public DbSet<WersItem> Movies { get; set; }
+    }
+}
