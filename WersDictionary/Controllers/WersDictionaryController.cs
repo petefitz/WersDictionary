d5aa86816d93be0acf595abf59c5975e94ae5581
Pete Fitzpatrick <pete@prf.demon.co.uk>
2014-02-17 09:45:29 +0000

    search criteria

--- WersDictionary/Controllers/WersDictionaryController.cs
+++ WersDictionary/Controllers/WersDictionaryController.cs
@@ -114,6 +114,34 @@ namespace WersDictionary.Controllers
             return RedirectToAction("Index");
         }
 
+        public ActionResult SearchIndex(string movieGenre, string searchString)
+        {
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Movies
+                           orderby d.FamilyCode
+                           select d.FamilyCode;
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreLst);
+
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.FamilyDescription.Contains(searchString));
+            }
+
+            if (string.IsNullOrEmpty(movieGenre))
+                return View(movies);
+            else
+            {
+                return View(movies.Where(x => x.FamilyCode == movieGenre));
+            }
+
+        }
+
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
